b3738745fe2da284672b090a829be12888f0a9bc
Bergs300 <37481102+Bergs300@users.noreply.github.com>
2021-02-07 15:49:55 +0100

    Add files via upload

---
+++ 21 01 31 homework_1.py
@@ -0,0 +1,47 @@
+"""
+Homework Assignment #1: Variables
+Listing of the metadata (attributes) from the song "Heaven Must Be Missing an Angel"
+"""
+# song name
+song_name = "Heaven Must Be Missing an Angel"
+
+# artist name
+artists = "Tavares"
+
+# album name
+album = "Sky High!"
+
+# recorded and released year
+released_year, re_released_year = 1976, 1986
+
+# song length in minutes
+duration_in_minutes = 6.38
+
+# genres
+genre = "Disco, Soul"
+
+# Songwriter(s)
+songwriter = "Freddie Perren and Keni St. Lewis"
+
+# Producer
+producer = "Freddie Perren"
+# label
+label = "Capitol"
+
+#chart performance
+dutch_top_40, us_hot_dance = 1, 1
+
+
+print('My favourite song is ', song_name, ".", sep="")
+print('Recorded by ', artists, ".", sep="")
+print('From the album:', album)
+print('It was released in the year', released_year)
+print('and re-released in ', re_released_year, ".", sep="")
+print('Genre is ', genre, ".", sep="")
+print('The lenght of the song is', duration_in_minutes, 'minutes.')
+print('Label: ', label, ".", sep="")
+print('Songwriter(s): ', songwriter, ".", sep="")
+print('Producer: ', producer, ".", sep="")
+print('Chart performance:')
+print('Dutch Top 40:', dutch_top_40)
+print('US Hot Dance Club Play:', us_hot_dance)
